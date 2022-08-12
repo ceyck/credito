6f123cfd63a2260cc1ac99cc0b72772e28bdea3c
Angel Arce <angel_ab86@outlook.com>
2022-08-11 21:54:15 -0500

    add slider buttons

--- main.js
+++ main.js
@@ -2,3 +2,5 @@ let button = document.querySelector("#button");
 
 let slider = document.querySelector("#slider");
 let sliderItems = document.querySelectorAll('.slider-items');
+let arrowButtonLeft = document.querySelector('.arrow-left');
+let awworButtonRight = document.querySelector('.arrow-t¿right');
